6a42bdfde4df88d4e4737aae7197c1565946e48d
Divinejay-001 <divinegiftjohnson21@gmail.com>
2024-11-22 12:23:01 -0800

    my poet

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -10,8 +10,8 @@ const Navbar = () => {
    <div className="desktopMenu">
 <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-100} duration={500} className='desktopMenuListItem'>Home</Link>
 <Link activeClass='active' to='skills' spy={true} smooth={true} offset={-100} duration={500} className='desktopMenuListItem'>About</Link>
-<Link activeClass='active' to='clients' spy={true} smooth={true} offset={-100} duration={500} className='desktopMenuListItem'>Clients</Link>
 <Link activeClass='active' to='works' spy={true} smooth={true} offset={-100} duration={500} className='desktopMenuListItem'>Portfolio</Link>
+<Link activeClass='active' to='clients' spy={true} smooth={true} offset={-100} duration={500} className='desktopMenuListItem'>Clients</Link>
     </div> 
     <button className="desktopMenuBtn" onClick={()=>{
       document.getElementById('contact').scrollIntoView({behavior: 'smooth'});
